cee04ced4ebd832fee075237c1b9fc58d9351aa1
Jenniffer <jlrlorente4@gmail.com>
2026-03-27 15:43:56 +0100

    Add SQL queries for data selection and filtering

---
+++ sql/select.sql
@@ -0,0 +1,30 @@
+Consultar todos los datos:
+  SELECT * FROM form;
+
+Filtrar por nombre ana.
+  SELECT * FROM form WHERE name='Ana';
+  
+Filtrar por nombre vacío.
+  SELECT * FROM form WHERE name IS NULL;
+  
+Filtrar por nombre que empiece por a.
+  SELECT * FROM form WHERE name LIKE 'A%';
+  
+Filtrar por edad entre 20 y 30.
+  SELECT * FROM form WHERE age BETWEEN 20 AND 30;
+  
+Filtrar por asunto job e information.
+
+  
+Filtrar por nombre maria o ana.
+
+  
+Filtrar por email que contenga gmail y asunto job.
+
+
+Filtrar por edad mayor a 30 y mensaje que contenga test.
+
+  
+Mostrar los datos ordenados por email de la A a la Z.
+
+
