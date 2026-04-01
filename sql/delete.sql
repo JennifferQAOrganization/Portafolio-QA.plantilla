7f3a975d9b3af4c33589dc94712e1d74be9c6fe7
Jenniffer <jlrlorente4@gmail.com>
2026-04-01 20:50:28 +0200

    Add delete commands for age and name in delete.sql
    Add SQL commands to delete records by age and name.

---
+++ sql/delete.sql
@@ -0,0 +1,7 @@
+Eliminar todos los registros con edad 30.  (No los he eliminado para que no se elmine de la tabla)
+  DELETE FROM form
+  WHERE age = 30;
+  
+Eliminar todos los registros con nombre pedro. (No los he eliminado para que no se elmine de la tabla)
+  DELETE FROM form
+  WHERE name = 'Pedro';
